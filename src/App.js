0e676366432fe84d21d504118337243f538e3348
Deborah Ong <deborah.oyt@gmail.com>
2021-08-21 23:28:43 -0700

    delete files and colons, add note

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import 'todomvc-app-css/index.css'
 import './App.css'
 
--- src/TodoFooter.js
+++ src/TodoFooter.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import classNames from 'classnames'
-
 import defs from './defs'
 import Utils from './Utils'
 
