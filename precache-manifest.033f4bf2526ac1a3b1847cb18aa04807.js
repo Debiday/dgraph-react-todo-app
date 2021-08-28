0502dfe3218b1bf0415a15cad9bca2340506742e
Deborah Ong <deborah.oyt@gmail.com>
2021-08-27 18:27:35 -0700

    Updates

---
+++ precache-manifest.033f4bf2526ac1a3b1847cb18aa04807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3930d12cfe5d5321ad06",
+    "url": "/dgraph-react-todo/static/js/main.fadabf41.chunk.js"
+  },
+  {
+    "revision": "cbc4c7a84222cb188b4b",
+    "url": "/dgraph-react-todo/static/js/runtime~main.b7cb7948.js"
+  },
+  {
+    "revision": "8942610c00def40503cb",
+    "url": "/dgraph-react-todo/static/css/2.6191ce91.chunk.css"
+  },
+  {
+    "revision": "8942610c00def40503cb",
+    "url": "/dgraph-react-todo/static/js/2.243ef7a4.chunk.js"
+  },
+  {
+    "revision": "21c521b9c09330dccd89fbcc8d25eff1",
+    "url": "/dgraph-react-todo/index.html"
+  }
+];
